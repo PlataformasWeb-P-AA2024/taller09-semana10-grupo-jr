29902654fcd3081ffea84f3cd69fedff17e2d98f
uuunin <rdcarrion3@utpl.edu.ec>
2024-06-17 20:37:35 -0500

    Finalización SqLite

--- deporteecuador/futbolec/admin.py
+++ deporteecuador/futbolec/admin.py
@@ -1,10 +1,38 @@
 from django.contrib import admin
 from .models import EquipoFutbol, Jugador, Campeonato, CampeonatoEquipos
 
-admin.site.register(EquipoFutbol)
+
+class EquipoFutbolAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'siglas', 'username_twitter',)
+    search_fields = ('nombre',)
+
+# admin.site
+admin.site.register(EquipoFutbol, EquipoFutbolAdmin)
+
+
+class JugadorAdmin(admin.ModelAdmin):
+
+    list_display = ('nombre', 'posicion_campo', 'numero_camiseta', 'sueldo', 'equipo_futbol')
+    search_fields = ('nombre', 'equipo_futbol')
+admin.site.register(Jugador, JugadorAdmin)
 
 
 
-admin.site.register(Jugador)
-admin.site.register(Campeonato)
-admin.site.register(CampeonatoEquipos)
+class CampeonatoAdmin(admin.ModelAdmin):
+
+    list_display = ('nombre', 'auspiciante',)
+    search_fields = ('nombre',)
+
+admin.site.register(Campeonato, CampeonatoAdmin)
+
+
+class EquiposCampeonatosAdmin(admin.ModelAdmin):
+    list_display = (
+'año',
+'equipo_futbol',
+'campeonato',
+    )
+    search_fields = ('equipo_futbol',)
+    
+
+admin.site.register(CampeonatoEquipos, EquiposCampeonatosAdmin)
